a4f49a86bcd9ff004ab5f257319a668044b10067
takao <ooshimatakao@ooshimatakaonoMacBook-Pro.local>
2020-05-06 16:24:07 +0900

    modify  matterjs

--- src/views/Matter.vue
+++ src/views/Matter.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="content">
+  <v-container>
     <div id="drawhere"></div>
-  </div>
+  </v-container>
 </template>
 
 <script>
 import Matter from "matter-js";
+import Kopen from "@/assets/ko2.png";
 
 export default {
-  data: () => ({}),
+  data: () => ({
+
+}),
   mounted() {
     // module aliases
     var Engine = Matter.Engine,
@@ -29,30 +32,37 @@ export default {
         width: width,
         height: height,
         pixelRatio: 2, //Pixel比; スマホ用に2にする
-        wireframes: false,
+        wireframes: false
         // background: '#fff'
-      },
-    });
-    console.log(render);
-    
-
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(width*0.1, 200, width * 0.15, width * 0.15, { frictionAir: 0.5 });
-    var boxB = Bodies.rectangle(width*0.15, 50, width * 0.15, width * 0.15, {
-      render: {
-        // fillStyle: "#fff123",
       }
     });
+    console.log(render);
+
+    // create two boxes and a ground
+    var boxA = Bodies.rectangle(width * 0.7, 100, width * 0.15, width * 0.15, {
+      frictionAir: 0.5
+    });
+    var boxB = Bodies.rectangle(100, 100, width * 0.15, width * 0.15, {
+      render: {
+        sprite: {
+          texture: Kopen,
+          xScale:2,
+          yScale:2
+        }
+      },
+    });
+    console.log(boxB);
+    
 
     // walls
     World.add(engine.world, [
       // Bodies.rectangle(400, 0, 800, 5, { isStatic: true }),
-      Bodies.rectangle(width*0.5, 550, width*0.9, 50, { isStatic: true })
+      Bodies.rectangle(width * 0.5, 550, width * 0.9, 50, { isStatic: true })
       // Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
       // Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
     ]);
 
-    World.add(engine.world, [boxA, boxB]);
+    World.add(engine.world, [boxA,boxB]);
 
     // add mouse control
     var mouse = Matter.Mouse.create(render.canvas),
@@ -101,23 +111,20 @@ export default {
 
     World.add(engine.world, [
       // cloth,
-      Bodies.circle(width * 0.6, height * 0.8, width * 0.1, { isStatic: true }),
+      Bodies.circle(width * 0.6, height * 0.8, width * 0.1, { isStatic: true })
       // Matter.Composites.newtonsCradle(width * 0.6, height * 0.5, 6, 20, 200)
       // Matter.Composites.pyramid(100, 258, 10, 10, 0, 0, function(x, y) {
       //   return Bodies.rectangle(x, y, 20, 20);
-      // })   
+      // })
     ]);
 
     // マウス押したら物体出現
-    Matter.Events.on(mouseConstraint,"mousedown",(e)=>{
+    Matter.Events.on(mouseConstraint, "mousedown", e => {
       // console.log(e,"down");
       World.add(engine.world, [
-      Matter.Bodies.rectangle(e.mouse.absolute.x,e.mouse.absolute.y,20,20)
-    ]);
-      
-
-      
-    })
+        Matter.Bodies.rectangle(e.mouse.absolute.x, e.mouse.absolute.y, 20, 20)
+      ]);
+    });
   }
 };
 </script>
